a9a4ef516d1ce08aa18ba37be219ecbd2f77320e
Alex Nolaço <38504933+AlexNolaco@users.noreply.github.com>
2023-05-02 23:54:53 -0300

    refatoração content component

--- src/app/layouts/content/content.component.ts
+++ src/app/layouts/content/content.component.ts
@@ -7,26 +7,18 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+
   constructor(private userService: UserService) { }
   aberto = false;
   tabelaPermissoes: any = [];
   pagina = "Home";
-  setPagina(pag: string) {
-    this.pagina = pag;
-  }
 
   ngOnInit() {
-    window.addEventListener('resize', function () {
-      var largura = window.innerWidth;
-      var a = document.getElementById("sidebarMenu");
-      if (a) {
-        if (largura > 750)
-          a.style.marginTop = "0px";
-        else
-          a.style.marginTop = "-30px";
-      }
-    });
+    this.setEventListners();
+    this.obterPermissoes();
+  }
 
+  obterPermissoes() {
     this.userService.permissoes().then(
       async (data: any) => {
         let filtrado = data.filter((x: any) => x.leitura == true);
@@ -37,39 +29,52 @@ export class ContentComponent implements OnInit {
             remocao: filtrado[index].remocao,
             edicao: filtrado[index].edicao,
           }
-
           this.tabelaPermissoes.push(objetoFinal);
         }
-
         localStorage.setItem("permissoes", JSON.stringify(this.tabelaPermissoes));
-        const aa: any = localStorage.getItem("permissoes");
         this.userService.log("Obter permissões");
       }
     );
   }
 
-  remove() {
+  setEventListners() {
+    window.addEventListener('resize', function () {
+      var largura = window.innerWidth;
+      var sideBar = document.getElementById("sidebarMenu");
+      if (sideBar) {
+        if (largura > 750)
+          sideBar.style.marginTop = "0px";
+        else
+          sideBar.style.marginTop = "-30px";
+      }
+    });
+  }
+
+  setarPagina(pag: string) {
+    this.pagina = pag;
+  }
+
+  logoff() {
     this.userService.log("Logoff no sistema");
     localStorage.clear();
   }
 
-  selecionaPermissao() {
-    const aa: any = localStorage.getItem("permissoes");
-    const valor = JSON.parse(aa).find((x: any) => x.tela == this.pagina);
+  selecionarPermissao() {
+    const permissoes: any = localStorage.getItem("permissoes");
+    const valor = JSON.parse(permissoes).find((x: any) => x.tela == this.pagina);
     return valor;
   }
 
-  mostraMenu() {
-    var a = document.getElementById("sidebarMenu");
-    if (a) {
+  mostrarMenu() {
+    var sideBar = document.getElementById("sidebarMenu");
+    if (sideBar) {
       if (!this.aberto) {
-        a.style.display = "block";
-        a.style.padding = "0px!important";
+        sideBar.style.display = "block";
+        sideBar.style.padding = "0px!important";
         this.aberto = true;
       }
-
       else {
-        a.style.display = "none";
+        sideBar.style.display = "none";
         this.aberto = false;
       }
     }
